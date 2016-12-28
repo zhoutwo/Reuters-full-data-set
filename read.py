f8265c3ac5fcbe01b5fa017560d894aa60fd1f3c
philipperemy <premy@reactive.co.jp>
2016-12-29 05:56:19 +0900

    compatibility with python 3

--- read.py
+++ read.py
@@ -2,16 +2,21 @@ from __future__ import print_function
 
 import os
 import pickle
+import sys
+
+PYTHON_3 = sys.version_info >= (3, 0)
 
 
 def read():
     for ls in os.listdir('data'):
         if ls.endswith('.pkl'):
-            f = open('data/' + ls, 'rb')
-            data = pickle.load(f)
-            for datum in data:
-                print('ts = {}, t = {}, h= {}'.format(datum['ts'], datum['title'], datum['href']))
-            f.close()
+            with open('data/' + ls, 'rb') as f:
+                if PYTHON_3:
+                    data = pickle.load(f, encoding='latin1')
+                else:
+                    data = pickle.load(f)
+                for datum in data:
+                    print('ts = {}, t = {}, h= {}'.format(datum['ts'], datum['title'], datum['href']))
 
 
 if __name__ == '__main__':
